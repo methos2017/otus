df978419d3cf5ce0ae1687c638b6f13500eb0efc
Sergey Peskov <sergeypeskov@Mac-mini-Sergey.local>
2018-12-06 16:46:30 +0300

    new server

--- lesson3/server.js
+++ lesson3/server.js
@@ -1,23 +1,7 @@
 const http = require('http')
-const hostname = '127.0.0.1'
 
-const port = 3000
-
-const server = http.createServer((req, res) => {
-
-    res.write('Hello\n');
-
+http.createServer((req, res) => {
     setTimeout(() => {
-
-
-        // res.statusCode = 200
-        // res.setHeader('Content-Type', 'text/plain')
-
-        res.end(' World!\n') }, 1000)
-})
-
-server.listen(port, hostname, () => {
-
-    console.log(`Server running at http://${hostname}:${port}`)
-
-})
+        res.end('Hello, World!')
+    }, 100)
+}).listen(3000)
