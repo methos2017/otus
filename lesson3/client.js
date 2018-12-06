68e8c304a66b83295aa5ce24e43da511da058baf
Sergey Peskov <sergeypeskov@Mac-mini-Sergey.local>
2018-12-06 08:41:45 +0300

    some work with resolve

--- lesson3/client.js
+++ lesson3/client.js
@@ -1,67 +1,78 @@
 const http = require('http');
 
-// const fetch = require("node-fetch");
 
-// var request = require("request");
+
 
 
 someGetWork = () => {
 
-    http.get('http://127.0.0.1:3000', (resp) => {
-        let data = '';
-      
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-      
-        // The whole response has been received. Print out the result.
+  // http.get('http://127.0.0.1:3000', (resp) => {
+  //   let data = '';
+  //   resp.on('data', (chunk) => {
+  //     data += chunk;
+  //   })
+  //   resp.on('end', () => {  
+  //     console.log('lets rock and parse some data: ' + data)
+  //   })
+  // }).on("error", (err) => {
+  //   console.log("Error: " + err.message);
+  // })
+
+  return new Promise(function (resolve, reject) {
+
+    http.get('http://127.0.0.1:3000', resp => {
+
+      let data = ''
+      resp.on('data', (chunk) => {
+        data += chunk;
+      })
+
+      resolve(
         resp.on('end', () => {
-          // console.log(JSON.parse(data).explanation);
-      
           console.log('lets rock and parse some data: ' + data)
-      
-        });
-      
-      }).on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
+        })
+
+      )
+    })
+
+    // http.get('http://127.0.0.1:3000', (resp) => {
+    //   let data = ''
+    //   resp.on('data', (chunk) => {
+    //     data += chunk
+    //   })
+    //   resp.on('end', () => {  
+    //     console.log('lets rock and parse some data: ' + data)
+    //   })
+    // }).on("error", (err) => {
+    //   console.log("Error: " + err.message)
+    // })
+
+  })
 }
 
 makeSequence = async (amount, mode) => {
 
-    let i
-    let res
+  let i
+  let res
 
 
+  for (i = 0; i < amount; i++) {
 
-      if (mode === 'parallel') {
+    if (mode === 'parallel') {
 
-        for (i = 0; i < amount; i++) {
+      someGetWork()
 
-
-          someGetWork()
-
-
-        }
-      }
-      else if(mode === 'onebyone') {
-
-        [...Array(amount)].reduce( (p, _, i) => 
-        p.then(_ => new Promise(resolve =>
-            setTimeout(function () {
-                // console.log(i)
-                someGetWork()
-                resolve()
-            }, Math.random() * 1000)
-        ))
-    , Promise.resolve() )
-        
-
-      }
     }
+    else if (mode === 'onebyone') {
+
+      await someGetWork()
 
 
+
+    }
+  }
+
+}
 
 
 
